5568b90a3ea04430660cc77d06513ad3ac0cad1b
Eugen Kobchuk <irravadi@gmail.com>
2023-04-10 08:09:16 +0300

    generation_2 done

--- Random_password_generation.py
+++ Random_password_generation.py
@@ -1,1 +1,62 @@
+import random
+import string
 
+while True:
+    try:
+        password_length = int(input("Enter password length: "))
+        if password_length < 1:
+            raise ValueError("Password length must be longer 0")
+        break
+    except ValueError as error:
+        print(f"Error: {error}")
+
+
+while True:
+    try:
+        use_special_chars = input("Use special characters? (y/n): ")
+        if use_special_chars.lower() == "y":
+            special_chars = string.punctuation
+            break
+        elif use_special_chars.lower() == "n":
+            special_chars = ""
+            break
+        else:
+            raise ValueError("Incorrect input")
+    except ValueError as error:
+        print(f"Error: {error}")
+
+while True:
+    try:
+        use_numbers = input("Use numbers? (y/n): ")
+        if use_numbers.lower() == "y":
+            numbers = string.digits
+            break
+        elif use_numbers.lower() == "n":
+            numbers = ""
+            break
+        else:
+            raise ValueError("Incorrect input")
+    except ValueError as error:
+        print(f"Error: {error}")
+
+while True:
+    try:
+        use_letters = input("Use letters? (y/n): ")
+        if use_letters.lower() == "y":
+            letters = string.ascii_letters
+            break
+        elif use_letters.lower() == "n":
+            letters = ""
+            break
+        else:
+            raise ValueError("Incorrect input")
+    except ValueError as error:
+        print(f"Error: {error}")
+
+
+all_chars = special_chars + numbers + letters
+
+
+password = "".join(random.choices(all_chars, k=password_length))
+
+print(f"Generated password: {password}")
